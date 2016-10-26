380e546bc0e2aa20bd6d94a8c4713aa2c165922e
mac <lpkai@macdeMacBook-Pro.local>
2016-10-26 19:03:59 +0800

    Initial Commit

---
+++ StepProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StepProgress
+//
+//  Created by liaopeikai on 2016/10/26.
+//  Copyright © 2016年 beichende. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StepProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StepProgress
+//
+//  Created by liaopeikai on 2016/10/26.
+//  Copyright © 2016年 beichende. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
